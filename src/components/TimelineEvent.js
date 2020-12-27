cb1b07bb4b85417125c9f079c18700ac4d81a69f
Rachael Gomez <gomezrc1220@gmail.com>
2020-12-27 13:25:20 -0800

    set up timeline event component

--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -4,8 +4,21 @@ import Timestamp from './Timestamp';
 import PropTypes from 'prop-types';
 
 const TimelineEvent = (props) => {
+  <div>
+    <ul>
+      <li>{props.person}</li>
+      <li>{props.event}</li>
+      <li>{props.timeStamp}</li>
+    </ul>
+  </div>
   
   return;
-}
+};
+
+TimelineEvent.propTypes = {
+  person: PropTypes.string.isRequired,
+  status: PropTypes.string.isRequired,
+  timeStamp: PropTypes.string.isRequired
+};
 
 export default TimelineEvent;
